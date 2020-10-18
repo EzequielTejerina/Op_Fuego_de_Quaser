54f6be0624a54fcecf917056b1f8623d239e36e6
lTejerina <eztejerina@alumno.unlam.edu.ar>
2020-10-17 22:15:56 -0300

    Cambios minimos para swagger

--- src/main/java/challenge/config/SwaggerConfig.java
+++ src/main/java/challenge/config/SwaggerConfig.java
@@ -1,11 +1,15 @@
 package challenge.config;
 
+import io.swagger.models.Path;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,13 +18,24 @@ import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig{
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.regex("/challenge.*"))
-                .build();
+                .build()
+                .apiInfo(apiInfo())
+                .tags(new Tag("CommunicationController", "Communication Controller"));
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Challenge Meli API",
+                "",
+                "1.0",
+                "",
+                null,
+                "", "", Collections.emptyList());
     }
 }
--- src/main/java/challenge/controller/CommunicationController.java
+++ src/main/java/challenge/controller/CommunicationController.java
@@ -3,19 +3,27 @@ package challenge.controller;
 import challenge.exception.ServiceException;
 import challenge.services.CommunicationService;
 import challenge.utils.ExceptionUtil;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET, RequestMethod.POST})
+@Api(tags = "CommunicationController")
 public class CommunicationController extends BaseController{
 
     @Autowired
     private CommunicationService communicationService;
 
     @PostMapping(value = "/topsecret/", consumes = JSON_UTF8, produces = JSON_UTF8)
+    @ApiOperation(
+            value = "TopSecret",
+            notes = "Retorna la ubicación de la nave y el mensaje que emite"
+    )
     public ResponseEntity<Object> topSecret(@RequestBody String requestBody) throws ServiceException {
         try{
             this.logger.info("Llamada al servicio /topsecret/ con el request body : {}", requestBody);
@@ -28,6 +36,10 @@ public class CommunicationController extends BaseController{
         }
     }
     @PostMapping(value = "/topsecret_split/{satellite_name}", consumes = JSON_UTF8, produces = JSON_UTF8)
+    @ApiOperation(
+            value = "TopSecretSplit",
+            notes = "Recibe la distancia a los satelite y el mensaje"
+    )
     public ResponseEntity<Object> topSecretSplitPost(@PathVariable(name = "satellite_name") String satelliteName, @RequestBody String requestBody) throws ServiceException {
         try{
             this.logger.info("Llamada al servicio post /topsecret_split/ con el request body : {}", requestBody);
@@ -40,6 +52,10 @@ public class CommunicationController extends BaseController{
         }
     }
     @GetMapping(value = "/topsecret_split/", produces = JSON_UTF8)
+    @ApiOperation(
+            value = "TopSecretSplit",
+            notes = "Retorna la ubicación de la nave y el mensaje que emite"
+    )
     public ResponseEntity<Object> topSecretSplitGet() throws ServiceException {
         try{
             this.logger.info("Llamada al servicio get /topsecret_split/");
